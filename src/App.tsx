11daa02485386c08ddd696649f91442d498e2f70
Pablo Chacón <pachacon@espol.edu.ec>
2026-07-06 19:34:41 +0000

    solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
 import './App.css'
 
 function App() {
@@ -7,10 +8,10 @@ function App() {
         <Grid container spacing={5} sx={{ justifyContent: "left", alignItems: "center" }}>
 
          {/* Encabezado */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado<HeaderUI/></Grid>
 
          {/* Alertas */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+         <Grid size={12}>Elemento: Alertas</Grid>
 
          {/* Selector */}
          <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
---
+++ src/components/HeaderUI.tsx
@@ -0,0 +1,9 @@
+import Typography from '@mui/material/Typography'
+
+export default function HeaderUI() {
+  return (
+    <Typography variant="h2" component="h1" sx={{ fontWeight: 'bold' }}>
+      Dashboard del Clima
+    </Typography>
+  )
+}
