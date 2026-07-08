e43b6013c97e0dbcfeec5169cf7e5463b191cf00
Pablo Chacón <pachacon@espol.edu.ec>
2026-07-08 19:11:48 +0000

    Componente SelectorUI aplicado con funciones de renderizacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid } from '@mui/material';
 import HeaderUI from './components/HeaderUI';
 import AlertUI from './components/AlertUI';
+import SelectorUI from './components/SelectorUI';
 import './App.css'
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
          <Grid size={12} container sx={{ justifyContent: "right", alignItems: "center" }}><AlertUI description="No se preveen lluvias"/></Grid>
 
          {/* Selector */}
-         <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
+         <Grid size={{ xs: 12, md: 3  }}><SelectorUI/></Grid>
 
          {/* Indicadores */}
          <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
